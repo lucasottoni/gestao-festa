9555ad0f2b858bda6073dfee91769f17e14b5218
Lucas <Lucas@SMBH-020425>
2017-01-21 12:14:03 -0200

    CHANGE ID FROM DEFINED_ENTITY TO SERIAL FOR POSTGRES

--- src/main/java/br/com/zup/model/DefinedEntity.java
+++ src/main/java/br/com/zup/model/DefinedEntity.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class DefinedEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	private String friendlyName;
